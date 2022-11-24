e5d0f2b605e554488ac85d3e442f22938200e6af
Artem Biryukov <abirukov2008@gmail.com>
2022-11-24 20:54:27 +0300

    anketa

--- utils.py
+++ utils.py
@@ -3,11 +3,13 @@ from random import randint, choice
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 import settings
 
+
 def get_smile(user_data):
     if 'emoji' not in user_data:
         smile = choice(settings.USER_EMOJI)
         return emojize(smile, language='alias')
     return user_data['emoji']
+
 
 def play_random_numbers(user_number):
     bot_number = randint(user_number - 10, user_number + 10)
@@ -19,7 +21,12 @@ def play_random_numbers(user_number):
         message = f'Ваше число {user_number}, мое {bot_number}. Вы проиграли'
     return message
 
+
 def main_keyboard():
     return ReplyKeyboardMarkup([
-        ['Прислать котика', KeyboardButton('Мои координаты', request_location=True)]
+        [
+            'Прислать котика',
+            KeyboardButton('Мои координаты', request_location=True),
+            'Заполнить анкету'
+        ]
     ])
